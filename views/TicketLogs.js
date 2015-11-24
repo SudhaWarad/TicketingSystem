03f93ae2e269c93722c0535171eb8bf9493ecf22
SudhaWarad <sudha123>
2015-11-25 00:49:37 +0530

    ticketlog

--- views/TicketLogs.js
+++ views/TicketLogs.js
@@ -5,52 +5,6 @@ myApp.config(['$httpProvider', function ($httpProvider) {
 	delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }]);0
 
-/*myApp.filter('sumByKey', function () {
-    return function (data, key) {
-        if (typeof (data) === 'undefined' || typeof (key) === 'undefined') {
-            return 0;
-        }
-        var sum = 0;
-        for (var i = data.length - 1; i >= 0; i--) {
-            sum += parseInt(data[i][key]);
-        }
-        return sum;
-    };
-})
-
-myApp.filter('customSum', function () {
-    return function (listOfProducts, key) {
-        //  Count how many items are in this order
-        var total = 0;
-        angular.forEach(listOfProducts, function (product) {
-            //            alert(product + "." + key);
-            total += eval("product." + key);
-        });
-        return total;
-    }
-});
-
-myApp.filter('countItemsInOrder', function () {
-    return function (listOfProducts) {
-        //  Count how many items are in this order
-        var total = 0;
-        angular.forEach(listOfProducts, function (product) {
-            total += product.Quantity;
-        });
-        return total;
-    }
-});
-
-myApp.filter('orderTotal', function () {
-    return function (listOfProducts) {
-        //  Calculate the total value of a particular Order
-        var total = 0;
-        angular.forEach(listOfProducts, function (product) {
-            total += product.Quantity * product.UnitPrice;
-        });
-        return total;
-    }
-});*/
 
 myApp.controller('TicketLogs',
 		function ($scope, $http) {
@@ -60,12 +14,9 @@ myApp.controller('TicketLogs',
 	$scope.selectedLog = null;
 	
 	$scope.resultPut = null
-	/* $http.get('http://localhost:3000/api/tickets',{"msg":"hi"}).success(function(data){
-    	 console.log(data);
-    });*/
+	
 
-
-	$http.get('http://localhost:3000/api/tickets', {headers:{ 'Content-Type': 'application/json'}})
+	$http.get('/api/tickets', {headers:{ 'Content-Type': 'application/json'}})
 	.success(function (data) {
 		$scope.listOfLogs = data;
 		if ($scope.listOfLogs.length > 0) {
@@ -97,7 +48,7 @@ myApp.controller('TicketLogs',
 	
 	$scope.gotoUpdate = function(val){
 		
-		$http.put("http://localhost:3000/api/tickets/"+ val._id,val).success(function(result) {
+		$http.put("/api/tickets/"+ val._id,val).success(function(result) {
 		      console.log(result);
 		      alert("Log updated Successfully");
 		      $scope.resultPut = result;
@@ -110,7 +61,7 @@ myApp.controller('TicketLogs',
 		$scope.listOftickets = null;
 
 
-		$http.get('http://localhost:3000/api/tickets')
+		$http.get('/api/tickets')
 		.success(function (data) {
 			$scope.listOftickets = data;
 		})
